e87f3fbd9d96aa485523295abd6f2db4a8c281fc
JackQA <41109425+JackQA@users.noreply.github.com>
2018-07-11 10:26:25 +0100

    Add files via upload

---
+++ build1.c
@@ -0,0 +1,54 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+
+#define RED   "\x1B[31m"
+#define GRN   "\x1B[32m"
+#define YEL   "\x1B[33m"
+#define RESET "\x1B[0m"
+
+int welcomeSocket, newSocket, nBytes;
+
+
+int main() {
+  srand(time(NULL));
+  struct sockaddr_in serverAddr;
+  struct sockaddr_storage serverStorage;
+  socklen_t addr_size;
+  char buffer[1024];
+
+  welcomeSocket = socket(AF_INET, SOCK_STREAM, 0);
+  serverAddr.sin_family = AF_INET;
+  serverAddr.sin_addr.s_addr = htons(INADDR_ANY);
+  serverAddr.sin_port = htons(4444);
+
+  bind(welcomeSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+
+  if (listen(welcomeSocket, 5) == 0) {
+    printf(GRN "Success" RESET " - Listening...\n");
+  } else {
+    printf(RED "Error" RESET " - Unable to start server.\n");
+    exit(0);
+  }
+
+  addr_size = sizeof serverStorage;
+  newSocket = accept(welcomeSocket, (struct sockaddr *) &serverStorage, &addr_size);
+
+  if(newSocket<0){
+    printf(RED "Error" RESET " - Error on binding!\n");
+    exit(0);
+  }
+  else{
+    printf(GRN "Success" RESET " - Client Attached.\n");
+  }
+  printf(YEL "Info" RESET " - Ending Session\n");
+  sleep(1);
+  close(newSocket);
+  close(welcomeSocket);
+  return 0;
+}
